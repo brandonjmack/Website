983e8bfb3089f50509f4d564df80f699cb0a142a
Brandon Mack <brandonjmack@protonmail.com>
2025-01-19 00:00:08 -0800

    changing nav again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Nameplate from "./Components/Nameplate/Nameplate";
 import AboutMe from "./Components/AboutMe/AboutMe";
@@ -9,7 +8,6 @@ import ContactMe from "./Components/Contact/Contact";
 
 const App = () => {
     return (
-        <Router>
             <div className="flex flex-col">
                 <Navbar />
                 <Nameplate />
@@ -17,18 +15,7 @@ const App = () => {
                 <Education />
                 <Projects />
                 <ContactMe />
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <div>
-
-                            </div>
-                        }
-                    />
-                </Routes>
             </div>
-        </Router>
     );
 };
 
