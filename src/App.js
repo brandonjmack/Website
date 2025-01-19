1159e9ac23272063a1a90acb701719775add0323
Brandon Mack <brandonjmack@protonmail.com>
2025-01-18 21:25:55 -0800

    changing from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar"
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 import Nameplate from "./Components/Nameplate/Nameplate";
 import AboutMe from "./Components/AboutMe/AboutMe";
-import Education from "./Components/Education/Education"
+import Education from "./Components/Education/Education";
 import Projects from "./Components/Projects/Projects";
-import ContactMe from "./Components/Contact/Contact"
+import ContactMe from "./Components/Contact/Contact";
 
 const App = () => {
     return (
-
         <Router>
             <div className="flex flex-col">
-                <Navbar/>
-                <Nameplate/>
+                <Navbar />
+                <Nameplate />
                 <Routes>
-                    <Route path="/" element={
-                        <div>
-                            <AboutMe/>
-                            <Education/>
-                            <Projects/>
-                            <ContactMe/>
-                        </div>
-                    }>
-                    </Route>
+                    <Route
+                        path="/"
+                        element={
+                            <div>
+                                <AboutMe />
+                                <Education />
+                                <Projects />
+                                <ContactMe />
+                            </div>
+                        }
+                    />
                 </Routes>
             </div>
         </Router>
-
-
-)
-    ;
+    );
 };
 
 export default App;
