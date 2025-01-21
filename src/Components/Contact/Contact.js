100e9c90fbb1d5feccc44293e709d4d583e74426
Brandon Mack <brandonjmack@protonmail.com>
2025-01-20 17:53:29 -0800

    changing the email

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,8 +7,7 @@ const Contact = () => {
         <div id="contact" className="contact-mainContainer">
             <div className="contact-textBox">
                 <h1 className="contact-header">Contact Me</h1>
-                <p className="contact-paragraph">
-                </p>
+                <p className="contact-paragraph">brandonjmack6@gmail.com</p>
                 <a href="https://www.linkedin.com/in/brandon-mack-15755a271/" className="contact-links">LinkedIn</a>
                 <a href="https://github.com/brandonjmack" className="contact-links">GitHub</a>
             </div>
